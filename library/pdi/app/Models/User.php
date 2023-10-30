0048fc77de59bb9e58532e4fb2322aee26e92b56
lfluizfelipe <lfluiz.felipelima@gmail.com>
2023-10-30 16:03:50 -0300

    first commit

---
+++ library/pdi/app/Models/User.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_name',
+        'login',
+        'password',
+        'user_type',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    /**
+     * Validation rules for the model's attributes.
+     *
+     * @var array
+     */
+    public static $rules = [
+        'user_name' => 'required',
+        'login' => 'required',
+        'password' => 'required',
+        'user_type' => 'required',
+    ];
+}
